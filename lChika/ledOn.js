573b704f6d0a65cb7133d150874f5c5cee406ff4
y-shinonome <y.shinonomail@gmail.com>
2022-03-02 21:40:05 +0900

    テストコード削除

--- lChika/ledOn.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var onoff_1 = require("onoff");
-var led = new onoff_1.Gpio(17, 'out');
-var iv = setInterval(function () { return led.writeSync(led.readSync() ^ 1); }, 200);
-setTimeout(function () {
-    clearInterval(iv);
-    led.unexport();
-}, 10200);
